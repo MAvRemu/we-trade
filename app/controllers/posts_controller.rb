5dcae2928b5d8f627dc6526ea81d4e57cc76bbd0
Tan Akpek akpektan@gmail.com <akpektan@gmail.com>
2022-08-29 15:52:41 +0200

    controllers set up

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,32 @@
 class PostsController < ApplicationController
+  def create
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def upvote
+  end
+
+  def unvote
+  end
+
+  def downvote
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
   resources :posts do
     resources :post_comments, only: [:create, :destroy]
   end
