6d1f97d43085f108939b5588d0b91e0b16b6a5b7
Marius van Remundt <marius_v_r@hotmail.com>
2022-08-31 15:27:30 +0200

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,22 +3,11 @@ require "json"
 
 #methods called below
 def download_cryptos
-
   Crypto.destroy_all
-  url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100"
-  cryptos_serialized = URI.open(url).read
-  cryptos = JSON.parse(cryptos_serialized)
-  output = []
-
-  cryptos.each do |c|
-    output << Crypto.create!(id_name: c["id"], name: c["name"], ticker: c["symbol"], price: c["current_price"], market_cap: c["market_cap"] , rank: c["market_cap_rank"], volume_24h: c["total_volume"], price_1d: c["price_change_percentage_24h"], image_url: c["image"])
-  end
-  output
+  UpdateCryptosJob.perform_now
 end
 
 def generate_crypto_comments(cryptos, marius)
-
-
   cryptos.each do |c|
     rand(2..10).times do
       CryptoComment.create!(user: marius, crypto: c, content_trix: '<div>Lorem ipsum dolor sit amet,&nbsp;<strong>consectetur adipiscing elit,</strong>&nbsp;<em>sed do eiusmod tempor incididunt</em>&nbsp;</div><ul><li>ut labore et dolore magna aliqua.&nbsp;</li><li>Ut enim ad minim veniam,&nbsp;</li><li>quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.&nbsp;</li></ul><div><a href="https://www.google.com/">Duis aute irure dolor in reprehenderit</a></div>')
@@ -36,7 +25,6 @@ def generate_crypto_ratings(cryptos, marius)
   end
 end
 
-
 # main seeds
 CryptoComment.destroy_all
 Post.destroy_all
@@ -47,7 +35,6 @@ User.destroy_all
 marius = User.new(email: "marius@hotmail.com", username: "Marius", password: "marius", admin: true)
 mantas = User.new(email: "mantas@hotmail.com", username: "Mantas", password: "mantas", admin: true)
 tan = User.new(email: "tan@hotmail.com", username: "Tan", password: "tantan", admin: true)
-
 
 marius.photo.attach(io: URI.open("http://res.cloudinary.com/dqiadt7pm/image/upload/v1661782386/hjqrltjashi2omdr2wbw.jpg"), filename: "marius.png", content_type: "image/png")
 tan.photo.attach(io: URI.open("http://res.cloudinary.com/dqiadt7pm/image/upload/v1661783149/hq4mhosbe1gjr0y4yunz.jpg"), filename: "tan.png", content_type: "image/png")
@@ -66,9 +53,10 @@ Post.create!(user: mantas, content: "ETH 2.0 why is it important and how can we 
 Post.create!(user: mantas, content: "The Metaverse, and my cryptos I am keeping an eye one ;)")
 puts "created posts"
 
-cryptos = download_cryptos
+download_cryptos
 puts "cryptos created"
 
+cryptos = Crypto.all
 generate_crypto_comments(cryptos, marius)
 puts "cryptos comments created"
 
@@ -76,16 +64,14 @@ generate_crypto_ratings(cryptos, marius)
 puts "cryptos ratings created"
 
 puts "creating squads"
-c_t = Chatroom.create()
-c_m = Chatroom.create()
-
+c_t = Chatroom.create
+c_m = Chatroom.create
 
 t = Squad.create!(title: "Ssup hoe", description: "BTC 100,000", user: tan, chatroom: c_t)
 puts "yas"
 m = Squad.create!(title: "E-money, reloaded", description: "We're a group of people trying to get rich quick", user: mantas, chatroom: c_m)
 
 puts "populating squads"
-
 
 Membership.create(squad: t, user: tan)
 Membership.create(squad: t, user: marius)
