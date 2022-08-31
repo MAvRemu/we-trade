f3917f869a3e3a85fe4c1ff622a0862bac5685a2
Tan Akpek akpektan@gmail.com <akpektan@gmail.com>
2022-08-31 17:19:45 +0200

    squad card beautify

--- app/models/squad.rb
+++ app/models/squad.rb
@@ -5,4 +5,8 @@ class Squad < ApplicationRecord
   has_one :chatroom, dependent: :destroy
   validates :user, presence: true
   validates :chatroom, presence: true
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :title, length: { minimum: 2 }
+  validates :title, length: { maximum: 12 }
 end
