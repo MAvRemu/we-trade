b87a4f9b7bdaf0628df15c16e488fb67fa92f5c6
Marius van Remundt <marius_v_r@hotmail.com>
2022-09-01 11:02:06 +0200

    videofixv1

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,8 @@ module WeTrade
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.assets.paths << "#{Rails.root}/app/assets/videos"
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
