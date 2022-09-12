9d695704bc7f9015f5e2405434cc35921251c22f
Marius van Remundt <marius_v_r@hotmail.com>
2022-09-12 12:46:56 +0200

    done

---
+++ app/policies/crypto_bookmarks_policy.rb
@@ -0,0 +1,16 @@
+class CryptoBookmarksPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      user.admin? ? scope.all : scope.where(crypto: record.crypto)
+    end
+  end
+
+  def create?
+    true
+  end
+
+  def destroy?
+    record.user == user || user.admin?
+  end
+end
